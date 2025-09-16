5654ee7f55d15812eb4856716ce59b361adcab65
t1anchen <143927+t1anchen@users.noreply.github.com>
2025-09-16 14:00:28 +0100

    feat: add split-at-time

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use std::{cell::RefCell, rc::Rc, string};
+use std::{cell::RefCell, rc::Rc};
 
 use anyhow::Ok;
 use clap::{Parser, Subcommand};
-use hello::Task;
 use tokio::{
   io::{AsyncBufReadExt, BufReader},
   process::Command,
@@ -48,19 +47,35 @@ enum Commands {
     #[arg(long)]
     split_video: bool,
   },
+  SplitByTime {
+    #[arg(long)]
+    start_time: String,
+
+    #[arg(long, default_value = "2")]
+    video_quality: u8,
+
+    #[arg(long, default_value = "image2")]
+    output_format: String,
+
+    #[arg(long, default_value = "1")]
+    video_frame: u8,
+
+    #[arg(long, default_value = ".png")]
+    output_suffix: String,
+  },
 }
 
 impl Opts {
   fn calibrate(&mut self) {
     match self.action {
       Some(Commands::SceneDetect {
-        detect_content,
-        list_scenes,
-        split_video,
+        detect_content: _,
+        list_scenes: _,
+        split_video: _,
       }) => {
         self.program = "scenedetect".to_owned();
       }
-      None => {}
+      _ => {}
     }
   }
   fn to_args(&self) -> Vec<String> {
@@ -68,11 +83,66 @@ impl Opts {
 
     match &self.program[..] {
       "ffmpeg" => {
+        // hide banner
+        args.push("-hide_banner".to_owned());
+        // input
         self.input_path.as_ref().map(|p| {
           args.push("-i".to_owned());
           args.push(p.clone());
         });
-        self.output_path.as_ref().map(|p| {
+        let mut output_path = self.output_path.clone();
+        // subcommands
+        match &self.action {
+          // split-by-time
+          Some(Commands::SplitByTime {
+            start_time,
+            video_quality,
+            output_format,
+            video_frame,
+            output_suffix,
+          }) => {
+            // misc and logging
+            args.push("-nostdin".to_owned());
+            args.push("-stats".to_owned());
+            args.push("-v".to_owned());
+            args.push("panic".to_owned());
+            // start time
+            args.push("-ss".to_owned());
+            args.push(start_time.clone());
+            // output video quality
+            args.push("-q:v".to_owned());
+            args.push(video_quality.to_string());
+            // output format
+            args.push("-f".to_owned());
+            args.push(output_format.clone());
+            // video frame
+            args.push("-vframes".to_owned());
+            args.push(video_frame.to_string());
+            // default output path
+            if output_path.is_none() {
+              let mut output_name = vec![];
+              self.input_path.as_ref().map(|p| {
+                let input_path = std::path::Path::new(&p);
+                input_path
+                  .file_stem()
+                  .map(|s| s.to_str())
+                  .flatten()
+                  .map(|stem_str| stem_str.to_string())
+                  .map(|stem| {
+                    output_name.push(stem);
+                  });
+              });
+
+              // output_name.push(start_time.to_string());
+              output_name.push(output_format.to_string());
+
+              output_path = Some(output_name.join("-") + output_suffix)
+            }
+          }
+          _ => {}
+        }
+        // output
+        output_path.as_ref().map(|p| {
           args.push(p.clone());
         });
       }
@@ -98,7 +168,7 @@ impl Opts {
               args.push("split-video".to_owned());
             }
           }
-          None => {}
+          _ => {}
         }
       }
       _ => {}
@@ -115,6 +185,7 @@ pub async fn api_main(mut opts: Opts) -> anyhow::Result<()> {
 
   if opts.borrow().dryrun {
     info!("{:?}", opts);
+    info!("{:?}", opts.borrow().to_args());
     return Ok(());
   }
 
